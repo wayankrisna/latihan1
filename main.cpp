f9a06f4739cb2a7afc150f7be373e03b4e346396
wayankrisna <krisnayasa2018@gmail.com>
2023-10-23 20:33:54 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "======================================" <<endl;
+    cout << "============= BIODATA ================" <<endl;
+    cout << "======================================" <<endl <<endl;
+    cout << " Nama : " << " I Wayan Krisna Yasa" <<endl;
+    cout << " Tempat Lahir :" << "Denpasar" <<endl;
+    cout << " Tanggal Lahir : " << "28 Februari 2005" <<endl;
+    cout << " Agama :" <<"Hindu" <<endl;
+    cout << " Jenis Kelamin : " << "Pria" <<endl;
+    cout << " Pekerjaan : " << "Mahasiswa" <<endl;
+    cout << " Usia : " << "18 tahun" <<endl;
+
+    return 0;
+}
