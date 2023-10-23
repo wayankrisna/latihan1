cefedddff0e5c14bf657da7ae5babfdcdd52a3e3
I Wayan Krisna Yasa <109776666+wayankrisna@users.noreply.github.com>
2023-10-23 20:36:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     cout << "======================================" <<endl <<endl;
     cout << " Nama : " << " I Wayan Krisna Yasa" <<endl;
     cout << " Tempat Lahir :" << "Denpasar" <<endl;
-    cout << " Tanggal Lahir : " << "28 Februari 2005" <<endl;
+    cout << " Tanggal Lahir : " << "28 Desember 2004" <<endl;
     cout << " Agama :" <<"Hindu" <<endl;
     cout << " Jenis Kelamin : " << "Pria" <<endl;
     cout << " Pekerjaan : " << "Mahasiswa" <<endl;
